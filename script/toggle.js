5127b2132da34452a3b26ccef3e47c5b3459cc2c
Umair505 <moinuli359@gmail.com>
2025-03-01 00:24:30 +0600

    added toggle

---
+++ script/toggle.js
@@ -0,0 +1,15 @@
+document.getElementById("add").style.display = "none";
+document.getElementById("cashout").style.display = "none";
+
+document.getElementById("add-btn").addEventListener("click",
+    function(){
+        document.getElementById("add").style.display = "block";
+        document.getElementById("cashout").style.display = "none";
+    }
+)
+document.getElementById("cashout-btn").addEventListener("click",
+    function(){
+        document.getElementById("cashout").style.display = "block";
+        document.getElementById("add").style.display = "none";
+    }
+)
