c6de6f236b8a2d274a0ffe05947d4e02e5ced068
Umair505 <moinuli359@gmail.com>
2025-03-01 10:48:21 +0600

    updated

--- script/cashout.js
+++ script/cashout.js
@@ -2,21 +2,31 @@ document.getElementById("cashout-button").addEventListener("click",
     function(event){
         event.preventDefault();
         const amount = document.getElementById("cashout-amount").value
+        const agentNumber = document.getElementById("agent-number").value
         const convertedAmount = parseFloat(amount)
         const mainBalance = document.getElementById("main-balance").innerText
         const updatedBalance = parseFloat(mainBalance);
         const pin = document.getElementById("cashout-pin").value;
-        if(amount && pin)
+        if(amount && pin && agentNumber.length == 11)
         {
-            if(pin == 1234 && updatedBalance >= convertedAmount)
+            if(pin == 1234)
             {
-                document.getElementById("main-balance").innerText = updatedBalance - convertedAmount;
-                alert("Cashout successful!")
+                if(updatedBalance >= convertedAmount)
+                {
+                    document.getElementById("main-balance").innerText = updatedBalance - convertedAmount;
+                    alert("Cashout successful!")
+                }
+                else{
+                    alert("Insufficient balance!")
+                }
             }
             else
             {
-                alert("Invalid PIN or insufficient balance!")
+                alert("Invalid PIN")
             }
+        }
+        else{
+            alert("Invalid inputs!")
         }
     }
 )
